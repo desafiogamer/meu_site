82848d4c29fd90ac611e625793d6bd4ddf88eac2
joao vitor <145628900+desafiogamer@users.noreply.github.com>
2023-10-24 01:18:37 -0300

    Update settings.py

--- aprendendo_css/settings.py
+++ aprendendo_css/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-l1yt*ot541q199t!9u6_@ps%4e-00o4geegu5sjl=*kf^-s7%9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh', '127.0.0.1', 'localhost']
 
 
 # Application definition
