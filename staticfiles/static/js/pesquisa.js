ed4921f4ad193c33c670e1e28981776fa4d7d371
joao vitor <145628900+desafiogamer@users.noreply.github.com>
2023-10-24 01:15:02 -0300

    Add files via upload

---
+++ staticfiles/static/js/pesquisa.js
@@ -0,0 +1,41 @@
+function filtrar() {
+    var input,
+        filter,
+        ul,
+        li,
+        a,
+        i,
+        span,
+        txtValue,
+        count = 0
+
+    input = document.getElementById('inputBusca');
+    ul = document.getElementById('listaLivros');
+
+    filter = input.value.toLocaleLowerCase();
+
+    li = ul.getElementsByTagName("li");
+
+    for(i = 0; i < li.length; i++){
+        a = li[i].getElementsByTagName("a")[0];
+        txtValue = a.textContent || a.innerText;
+        if(txtValue.toLocaleLowerCase().indexOf(filter) > -1){
+            li[i].style.display = "";
+            count++
+            span = li[i].querySelector(".item-name");
+            if(span){
+                span.innerHTML = txtValue.replace(new RegExp(filter, "gi"),(match)=>{
+                    return "<strong>" + match + "</strong>";
+                })
+            }
+        }else{
+            li[i].style.display = "none";
+        }
+    }
+
+    if (count == 0){
+        ul.style.display = "none";
+    }else{
+        ul.style.display = "block";
+    }
+}
