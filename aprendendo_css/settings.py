47b74561d84aa87d616cefb2aa124dbe76df5155
joao vitor <145628900+desafiogamer@users.noreply.github.com>
2023-10-24 01:53:41 -0300

    Update settings.py

--- aprendendo_css/settings.py
+++ aprendendo_css/settings.py
@@ -130,8 +130,6 @@ MEDIA_ROOT = (
 )
 
 STATIC_URL = 'static/'
-STATIC_ROOT = 'staticfiles'
-
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
