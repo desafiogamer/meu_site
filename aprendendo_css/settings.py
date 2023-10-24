9956000b3abada9ddaed0510594a8bb9bb2a00b5
joao vitor <145628900+desafiogamer@users.noreply.github.com>
2023-10-24 01:45:07 -0300

    Update settings.py

--- aprendendo_css/settings.py
+++ aprendendo_css/settings.py
@@ -130,15 +130,18 @@ MEDIA_ROOT = (
 )
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles", 'static')
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
+STATIC_ROOT = 'staticfiles'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles", 'static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
 
 LOGIN_REDIRECT_URL = 'index'
 LOGIN_URL = 'login'
